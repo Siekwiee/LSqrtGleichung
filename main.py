8cb424b7102e2948c8e698ae7d95134bfe81a3c7
Jannik_Wiest <jannik.wiest@gmail.com>
2023-12-07 08:55:07 +0100

    Init

---
+++ main.py
@@ -0,0 +1,12 @@
+from math import *
+
+def Welcome():
+    print("\nHerzlich wilkommen, mit diesem Programm wirst du in der lage sein die quadratische Gleichung(ax^2 + bx + c = 0) zu lösen")
+    print("---------------------------------------------------------------------------------------------------------------------------")
+
+def UserInput():
+    a = float(input("Koeffizient a: "))
+    b = float(input("Koeffizient b: "))
+    c = float(input("Koeffizient c: "))
+
+UserInput()
